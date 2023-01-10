8fe58daa136508d818d0d2ae9d8580a94e219833
Olebogeng H. Mohlachwa <OMohlachwa@bitri.co.bw>
2023-01-10 11:34:17 +0200

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,81 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles, isValidImage, isValidUrl} from './util/util';
+import { validateImage }  from 'image-validator';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  app.get ('/filteredimage', async (req: express.Request, res: express.Response) => {
+    //
+    let { image_url }  = req.query;
+
+    if (image_url) {
+
+      if(!isValidUrl(image_url)){
+
+        return res.status(400).send({ error: "Bad Request, image_url is invalid" });
+      
+      }else if (!isValidImage(image_url)){
+
+        return res.status(400).send({ error: "image_url is invalid, should be in a form of (jpeg|jpg|gif|png)" });
+
+      }else {
+
+          try {
+            filterImageFromURL(image_url).then((response) => {
+              res.status(200).sendFile(response, async () =>{
+                 await deleteLocalFiles([response]);
+              });
+            });      
+        }catch (error){
+          return res.status(500).send({error: "Enternal Server Error, image can't be processed"});
+      }
+    }
+
+    } else {
+      res.status(404).send("URL not found");
+    }
+
+  });
+
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
